7dbf825d4666c1eed378e7fded5cb0568051fd26
Andrew Jones <ascjones@gmail.com>
2021-11-24 13:54:32 +0000

    Pass args on command line

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,101 @@
 #[subxt::subxt(runtime_metadata_path = "relay-chain.scale")]
 pub mod relay_chain {}
 
-use relay_chain::runtime_types::xcm::{
-    VersionedMultiAssets,
-    VersionedMultiLocation,
-    v0::{
-        junction::{
-            Junction,
-            NetworkId,
+use relay_chain::{
+    DefaultConfig,
+    runtime_types::xcm::{
+        VersionedMultiAssets,
+        VersionedMultiLocation,
+        v0::{
+            junction::{
+                Junction,
+                NetworkId,
+            },
+            multi_location::MultiLocation as V0MultiLocation,
         },
-        multi_location::MultiLocation as V0MultiLocation,
-    },
-    v1::{
-        multiasset::{
-            AssetId,
-            MultiAsset,
-            MultiAssets,
-            Fungibility,
-        },
-        multilocation::{
-            Junctions,
-            MultiLocation as V1MultiLocation
-        },
+        v1::{
+            multiasset::{
+                AssetId,
+                MultiAsset,
+                MultiAssets,
+                Fungibility,
+            },
+            multilocation::{
+                Junctions,
+                MultiLocation as V1MultiLocation
+            },
+        }
     }
 };
 
-use color_eyre::eyre::WrapErr;
-use sp_keyring::AccountKeyring;
-use subxt::{ClientBuilder, PairSigner};
+use color_eyre::eyre::{
+    self,
+    WrapErr
+};
+use sp_core::{crypto::Pair, sr25519};
+use subxt::{Config, ClientBuilder, PairSigner};
+use structopt::StructOpt;
+
+/// CLI for submitting XCM messages.
+#[derive(Debug, StructOpt)]
+struct Opts {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Teleport an asset to another parachain.
+    #[structopt(name = "teleport")]
+    TeleportAsset {
+        /// The id of the destination parachain.
+        #[structopt(long, short)]
+        parachain_id: u32,
+        /// The account on the destination parachain to which.
+        #[structopt(long, short)]
+        dest_account: <DefaultConfig as Config>::AccountId,
+        /// The amount of the (fungible) asset to transfer.
+        #[structopt(long, short)]
+        amount: u128,
+        #[structopt(flatten)]
+        extrinsic_opts: ExtrinsicOpts,
+    }
+}
+
+/// Arguments required for creating and sending an extrinsic to a substrate node
+#[derive(Clone, Debug, StructOpt)]
+pub(crate) struct ExtrinsicOpts {
+    /// Websockets url of a substrate node
+    #[structopt(
+        name = "url",
+        long,
+        parse(try_from_str),
+        default_value = "ws://localhost:9944"
+    )]
+    url: url::Url,
+    /// Secret key URI for the account deploying the contract.
+    #[structopt(name = "suri", long, short)]
+    suri: String,
+    /// Password for the secret key
+    #[structopt(name = "password", long, short)]
+    password: Option<String>,
+}
+
+impl ExtrinsicOpts {
+    pub fn signer(&self) -> color_eyre::Result<sr25519::Pair> {
+        sr25519::Pair::from_string(&self.suri, self.password.as_ref().map(String::as_ref))
+            .map_err(|_| eyre::eyre!("Secret string error"))
+    }
+}
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let signer = PairSigner::new(AccountKeyring::Alice.pair());
+    let opts = Opts::from_args();
+
+    let Command::TeleportAsset { parachain_id, dest_account, amount, extrinsic_opts } = opts.command;
+    let signer = PairSigner::new(extrinsic_opts.signer()?);
 
     let api = ClientBuilder::new()
         .set_url("ws://localhost:9944")
@@ -42,16 +104,11 @@ async fn main() -> color_eyre::Result<()> {
         .context("Error connecting to substrate node")?
         .to_runtime_api::<relay_chain::RuntimeApi<relay_chain::DefaultConfig>>();
 
-    // todo: check and pass args
-    let dest_parachain = 1002;
-    let dest_account = AccountKeyring::Bob.to_account_id().into();
-    let amount = 1_000_000_000_000;
-
     let dest = VersionedMultiLocation::V0(
-        V0MultiLocation::X1(Junction::Parachain(dest_parachain))
+        V0MultiLocation::X1(Junction::Parachain(parachain_id))
     );
     let beneficiary = VersionedMultiLocation::V0(
-        V0MultiLocation::X1(Junction::AccountId32 { network: NetworkId::Any, id: dest_account })
+        V0MultiLocation::X1(Junction::AccountId32 { network: NetworkId::Any, id: dest_account.into() })
     );
     let assets = VersionedMultiAssets::V1(
         MultiAssets(
